9ae1f2009eb0c215995cf6eeaff41627bb52ca48
aldys002 <igidiaaldys@gmail.com>
2024-05-14 15:21:06 +0700

    menambah tiitk koma pada akhir kurung kurawal class mahasiswa

---
+++ constructor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa 
+{
+public : 
+    int nim;
+    string nama;
+
+public : 
+   Mahasiswa ()
+   {
+    nim = 0;
+    nama = "";
+   }
+};
--- mbrStaticfunc.cpp
+++ mbrStaticfunc.cpp
@@ -55,4 +55,6 @@ int main()
     mhs4.printAll();
 
     cout << "akses dari luar object = " << mahasiswa::getNim() << endl; // mengakses nim melalui static member function "getNim "
+
+    return 0;
 }
