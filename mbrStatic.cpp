28d3147f38b7f078d7aa393245644441b1c57176
aldys002 <igidiaaldys@gmail.com>
2024-05-14 14:45:05 +0700

    membuat fungsi utama

--- mbrStatic.cpp
+++ mbrStatic.cpp
@@ -23,5 +23,17 @@ int mahasiswa::nim = 207;
 
 void mahasiswa::setID()
 {
+    id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
     
 }
