aa07b38948df4003d34b34416b18a54525d732c4
aldys002 <igidiaaldys@gmail.com>
2024-05-14 15:41:47 +0700

    membuat class angka

--- constructor.cpp
+++ constructor.cpp
@@ -12,7 +12,7 @@ public :
    Mahasiswa ()
    {
     nim = 0;
-    nama = "";
+    nama = " ";
    };
 
 
@@ -37,5 +37,15 @@ public :
 
 int main()
 {
-    
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Indra");
+    Mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
 }
