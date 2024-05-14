80673a932820c258259a498808f571dd7d721e47
aldys002 <igidiaaldys@gmail.com>
2024-05-14 15:51:47 +0700

    membuat prosedur angka

--- destructor.cpp
+++ destructor.cpp
@@ -9,4 +9,30 @@ private :
 public :
     angka(int); //Constructor
     ~angka(); //Destructor
+    void cetakData();
+    void isiData();
+};
+//Definisi member Function
+
+angka::angka(int i){
+    panjang = 1;
+    arr = new int[1];
+    isiData();
+}
+
+angka::~angka(){
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout<<"Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData(){
+    for(int i=1; i<=panjang; i++){
+        cout << i << " = " << arr[1] << endl;
+    }
+}
+
+void angka::isiData(){
+    
 }
